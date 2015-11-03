8f12eaa966c9220d8ce5de36b12a0071dbd5ffb9
Fabio Collini <fabio.collini@gmail.com>
2015-11-03 22:31:31 +0100

    Watch-phone communication (cherry picked from commit 0c6cfb7)

---
+++ mobile/src/main/java/it/cosenonjaviste/mywearapp/MyService.java
@@ -0,0 +1,22 @@
+package it.cosenonjaviste.mywearapp;
+
+import com.mariux.teleport.lib.TeleportService;
+
+import it.cosenonjaviste.firebaseconnector.SurveyManager;
+
+public class MyService extends TeleportService {
+    private SurveyManager surveyManager;
+
+    @Override public void onCreate() {
+        super.onCreate();
+        surveyManager = new SurveyManager(this);
+        setOnGetMessageCallback(new MyOnGetMessageCallback());
+    }
+
+    private class MyOnGetMessageCallback extends OnGetMessageCallback {
+        @Override public void onCallback(String s) {
+            surveyManager.updateAnswer(s);
+            setOnGetMessageCallback(new MyOnGetMessageCallback());
+        }
+    }
+}
--- wear/src/main/java/it/cosenonjaviste/mywearapp/MainActivity.java
+++ wear/src/main/java/it/cosenonjaviste/mywearapp/MainActivity.java
@@ -2,12 +2,38 @@ package it.cosenonjaviste.mywearapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.mariux.teleport.lib.TeleportClient;
 
 public class MainActivity extends Activity {
+
+    private TeleportClient teleportClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.answer);
+        teleportClient = new TeleportClient(this);
+        findViewById(R.id.answer_y).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                teleportClient.sendMessage("yes", null);
+            }
+        });
+        findViewById(R.id.answer_n).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                teleportClient.sendMessage("no", null);
+            }
+        });
+    }
+
+    @Override protected void onStart() {
+        super.onStart();
+        teleportClient.connect();
+    }
+
+    @Override protected void onStop() {
+        super.onStop();
+        teleportClient.disconnect();
     }
 }
